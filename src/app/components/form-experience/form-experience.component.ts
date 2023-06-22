6bd07082268d83ad482cc9cfd053eb23f15f184a
brunodev <emaildobrunocaike@gmail.com>
2023-06-22 18:45:49 -0300

    adicionado um form na lp

--- src/app/components/form-experience/form-experience.component.ts
+++ src/app/components/form-experience/form-experience.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-experience',
   templateUrl: './form-experience.component.html',
   styleUrls: ['./form-experience.component.scss']
 })
+
+
 export class FormExperienceComponent implements OnInit {
+  experienceForm!: FormGroup;
+  finally: boolean = false;
+  @Input() experiences: { title: string; content: string; }[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.experienceForm = new FormGroup({
+      title: new FormControl('', [Validators.required]),
+      content: new FormControl('', [Validators.required])
+    });
+  }
+
+  get title() {
+    return this.experienceForm.get('title')!;
+  }
+
+  get content() {
+    return this.experienceForm.get('content')!;
+  }
+
+  submit() {
+    if (this.experienceForm.invalid) {
+      return;
+    }
+    this.experiences.push({ title: this.title.value, content: this.content.value });
+    this.finally = true;
   }
 
 }
