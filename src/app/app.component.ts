820ce56112a9e983da499f0805e079beddbe888c
brunodev <emaildobrunocaike@gmail.com>
2023-06-21 21:17:18 -0300

    adicionado o footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ export class AppComponent {
     'Comunicativo', 'Trabalho em equipe', 'Princípios éticos', 'Confiança', 'Atitude positiva', 'Proatividade', 'Flexibilidade'
   ];
   hards = [
-    'HTML - 3 anos', 'CSS - 3 anos', 'SASS - 2,5 anos', 'Bootstrap - 2 anos', 'Tailwind - 1,5 anos', 'Javascript - 3 anos', 'ReactJS - 1 ano', 'PHP - 3 anos', 'Laravel - 3 anos', 'Lumen - 3 anos', 'SQL - 3 anos', 'MYSQL - 3 anos', 'GIT - 3 anos', 'GitLab - 3 anos', 'GitHub - 3 anos'
+    'HTML - 3 anos', 'CSS - 3 anos', 'SASS - 2,5 anos', 'Bootstrap - 2 anos', 'Tailwind - 1,5 anos', 'Javascript - 3 anos', 'ReactJS - 1 ano', 'PHP - 3 anos', 'Laravel - 3 anos', 'Lumen - 3 anos', "Wordpress - 6 meses", 'SQL - 3 anos', 'MYSQL - 3 anos', 'GIT - 3 anos', 'GitLab - 3 anos', 'GitHub - 3 anos'
   ];
   courses = [
     'Codeboost', 'Scrum Básico', 'Git e GitHub', 'Básico Power BI', 'Laravel Developer', 'FullStack PHP Developer', 'Javascript Origamid', 'Anáise e Desenvolvimento de Sistemas'
